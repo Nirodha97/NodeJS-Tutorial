f85ffd709a715a7619e5a90e7026d3c0a9b3c103
Nirodha97 <nirodhamwickramaratne@gmail.com>
2022-07-05 11:57:04 +0530

    Created Express App

---
+++ 05-express-first-app/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET users listing. */
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+router.get('/users/details', function(req, res, next) {
+  res.send('Details');
+});
+
+module.exports = router;
